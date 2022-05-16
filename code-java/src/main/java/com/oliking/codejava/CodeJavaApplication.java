e034e16846d82e616da98507ab1bd798107e33a3
OliKing <sxl144012@163.com>
2022-05-16 22:54:06 +0800

    创建

---
+++ code-java/src/main/java/com/oliking/codejava/CodeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.oliking.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeJavaApplication.class, args);
+    }
+
+}
